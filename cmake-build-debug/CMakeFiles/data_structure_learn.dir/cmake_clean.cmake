d2e0bc78d83890bf4f203b4cd463158909869cef
Zhaoyilunnn <40353317+Zhaoyilunnn@users.noreply.github.com>
2020-02-22 23:44:35 +0800

    update

---
+++ cmake-build-debug/CMakeFiles/data_structure_learn.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/data_structure_learn.dir/main.cpp.o"
+  "CMakeFiles/data_structure_learn.dir/struc_func.cpp.o"
+  "data_structure_learn.exe"
+  "data_structure_learn.pdb"
+  "libdata_structure_learn.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/data_structure_learn.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
