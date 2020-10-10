ab266f39c9a619829c03f1deb48da35598355eaf
Zhaoyilunnn <dilved1014@gmail.com>
2020-10-10 22:53:12 +0800

    update

---
+++ cmake-build-debug/CMakeFiles/data_structure_learn.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/data_structure_learn.dir/algorithm.cpp.o"
+  "CMakeFiles/data_structure_learn.dir/graph.cpp.o"
+  "CMakeFiles/data_structure_learn.dir/main.cpp.o"
+  "CMakeFiles/data_structure_learn.dir/sort.cpp.o"
+  "CMakeFiles/data_structure_learn.dir/tree.cpp.o"
+  "data_structure_learn.exe"
+  "data_structure_learn.pdb"
+  "libdata_structure_learn.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/data_structure_learn.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
